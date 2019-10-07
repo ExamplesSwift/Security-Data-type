71f467c6077cce40cda9efbb64e2e152556bb1ba
Henry AT <yiperu@gmail.com>
2019-10-06 23:34:29 -0300

    With this have the memory direcction of unsafe way

--- Data_Playground.playground/Contents.swift
+++ Data_Playground.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 var string = "Hello, I am a string"
 
-guard let dataString = string.data(using: .utf8) else {
+guard var dataString = string.data(using: .utf8) else {
     fatalError()
 }
 dataString
@@ -10,8 +10,12 @@ dataString
 let data64 = dataString.base64EncodedData(options: .lineLength64Characters)
 let string64 = dataString.base64EncodedString(options: .lineLength64Characters)
 // Come back
-
 let normalData = Data(base64Encoded: data64)
 let normalDataC64 = Data(base64Encoded: string64, options: .ignoreUnknownCharacters)
-
 String(data: normalDataC64!, encoding: .utf8)
+
+// Access to Array
+dataString.withUnsafeBytes { (data: UnsafePointer<Int>) in
+    print(data)
+}
+
