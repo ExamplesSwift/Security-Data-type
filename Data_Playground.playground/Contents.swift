4b2d2a72497cea9c35b113ea87ba0057c75d5855
Henry AT <yiperu@gmail.com>
2019-10-06 23:06:49 -0300

    Become to Base64

--- Data_Playground.playground/Contents.swift
+++ Data_Playground.playground/Contents.swift
@@ -9,3 +9,8 @@ dataString
 // Now become to Base64
 let data64 = dataString.base64EncodedData(options: .lineLength64Characters)
 let string64 = dataString.base64EncodedString(options: .lineLength64Characters)
+// Come back
+
+let normalData = Data(base64Encoded: data64)
+let normalDataC64 = Data(base64Encoded: string64, options: .ignoreUnknownCharacters)
+
